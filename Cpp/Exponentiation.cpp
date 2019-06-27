ae89717468e632d02452f5babc0b30417510f37e
Achillece <51154010+Achillece@users.noreply.github.com>
2019-06-27 23:10:52 +0800

    first update

---
+++ Cpp/Exponentiation.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include<cmath>
+int main()
+{
+	double a; int b;
+	while (std::cin >> a >> b) {
+		if (a > 0 && b > 0 && a < 99.999 && b <= 25) {
+			std::cout << pow(a, b) << std::endl;
+		}
+		else return 0;
+	}
+	return 0;
+}
